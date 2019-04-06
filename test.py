a4c994a898dee7eb39e92b5b73d03bd324362876
oriolaranda <oriolaranda@gmail.com>
2019-04-06 18:43:35 +0200

    oriol-commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from joblib import load
+from karea
+
+
+
+def test(img):
+    #load cnn
+    model = load('./trained_cnn.joblib')
+    model.
+def main():
+    test()
+
+main()
