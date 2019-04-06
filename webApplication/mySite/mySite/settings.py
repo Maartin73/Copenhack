64a49df42d6772cb0476468932e7c2febcb23a92
David Pujol <david.pujolperich@gmail.com>
2019-04-06 19:08:00 +0200

    Projecte Django funcionant.

--- webApplication/mySite/mySite/settings.py
+++ webApplication/mySite/mySite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'signRecognizer',
 ]
 
 MIDDLEWARE = [
@@ -49,12 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'webApplication.urls'
+ROOT_URLCONF = 'mySite.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'signRecognizer/template')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
